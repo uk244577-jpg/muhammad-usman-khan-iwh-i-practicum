3e7832d990edaaa4cb5f6a6074bf1feb718474ea
Muhammad Usman Khan <l252003@lhr.nu.edu.pk>
2026-06-20 18:43:33 +0500

    Add second and third and make changes from one

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -13,22 +14,46 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
-app.get('/',async (req, res) => {
+app.get('/', async (req, res) => {
     const url = 'https://api.hubapi.com/crm/v3/objects/2-231419013?properties=name,author,genre';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
-})
+    try {
+        const resp = await axios.get(url, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Books | HubSpot Practicum', data });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
-
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', {
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+    });
+});
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-
+app.post('/update-cobj', async (req, res) => {
+    const { name, author, genre } = req.body;
+    const url = 'https://api.hubapi.com/crm/v3/objects/2-231419013';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+    try {
+        await axios.post(url, { properties: { name, author, genre } }, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+    }
+});
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
